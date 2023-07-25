d773ed49050be07090b8d81d73861bfbe1dc5614
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2023-07-25 15:23:39 +0200

    puzzle new version

---
+++ puzzle/assets/summer2023_3x3-d869c9e3.js
@@ -0,0 +1,1 @@
+const v={type:"coloredSquares",size:3,pieces:["lrv^_POYG","lrv^_POGY","lr^v_PORB","lr^v_POBR","-^v^_TRMG","-^v^_TBMY","-v^v_MGTB","-v^v_MYTR","v^v^_TMTM"],background:["ROBTGPY","BORMYPG","ROBTGPY","BORMYPG"]};export{v as default};
